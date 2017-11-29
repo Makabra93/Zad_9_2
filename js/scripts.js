79fd1c5a9b891b611f79ab914761bde31d0d53b9
Mateusz Lawczys <ma6tek@gmail.com>
2017-11-29 20:52:38 +0100

    Add variables, Code test

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,5 @@
 // scripts.js
 
-// OPCJA PIERWSZA: 
-
-function getTriangleArea(a, h) {
-    if ( a <= 0 || h <= 0 ) {
-        return 'Nieprawidłowe dane';
-    }
-
-    return a*h/2;  
-}
-
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(30, 52);
-var triangle3Area = getTriangleArea(-5, 10);
-
-console.log('1 Pole = ' + triangle1Area);
-console.log('2 Pole = ' + triangle2Area);
-console.log('3 Pole = ' + triangle3Area);
-
-
-// OPCJA DRUGA :
-
-function getTriangle2Area(a, h) {
-return (a > 0 && h > 0) ? a * h / 2 : "nieprawidlowe dane";
-}  
-
-console.log(getTriangle2Area(10, 15));
+var maleNames = ['Marek', 'Zbigniew', 'Mateusz', 'Gabriel', 'Ignacy'];
+var femaleNames = ['Kasia', 'Ania', 'Patrycja', 'Zosia', 'Klara'];
+var allNames = maleNames.concat(femaleNames);
